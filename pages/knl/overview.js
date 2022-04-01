781a31d070103e64c9b9058598b9750b57c7ce83
Fabio Riva <f.riva@sotefin.ch>
2022-04-01 17:18:35 +0200

    add aps knl/knr

---
+++ pages/knl/overview.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import fetch from 'src/lib/fetch'
+import authSSR from 'src/lib/authSSR'
+import { OVERVIEW } from '/src/constants/auth'
+import Overview from 'src/components/overview/kn'
+import withAuthSync from 'src/hocs/withAuthSync'
+
+const Page = props => <Overview {...props} />
+
+export async function getServerSideProps (ctx) {
+  const hrstart = process.hrtime()
+
+  const props = await authSSR(ctx, 'knl', OVERVIEW)
+  if (props.notFound || props.redirect) return props
+
+  const url = `${process.env.NEXT_PUBLIC_BACKEND_URL}/knl/overview`
+  const json = await fetch(url, {
+    headers: { Authorization: 'Bearer ' + props.token }
+  })
+
+  const hrend = process.hrtime(hrstart)
+
+  return { props: { ...props, json, executionTime: hrend } }
+}
+
+export default withAuthSync(Page)
--- src/components/overview/kn.js
+++ src/components/overview/kn.js
@@ -1,73 +1,83 @@
-import React from "react";
-import useTranslation from "next-translate/useTranslation";
-// import Box from "@mui/material/Box";
-import Grid from "@mui/material/Grid";
-import Error from "src/components/Error";
-import Device from "src/components/overview/DeviceReduced";
-import OperationDialog from "src/components/overview/OperationDialog";
-import Queue from "src/components/overview/Queue";
-import fetch from "src/lib/fetch";
-import { useData } from "src/lib/useWebSocket";
-import { ACTIONS, isAllowed } from "/src/constants/auth";
+import React from 'react'
+import useTranslation from 'next-translate/useTranslation'
+import Grid from '@mui/material/Grid'
+import Error from 'src/components/Error'
+import Layout from 'src/components/Layout'
+import Message from 'src/components/Message'
+import Device from 'src/components/overview/DeviceReduced'
+import OperationDialog from 'src/components/overview/OperationDialog'
+import Queue from 'src/components/overview/Queue'
+import fetch from 'src/lib/fetch'
+import { useData } from 'src/lib/useWebSocket'
+import { ACTIONS, isAllowed } from '/src/constants/auth'
 
-export default function Overview(props) {
-  const { t } = useTranslation("overview");
+export default function Overview (props) {
+  const { t } = useTranslation('overview')
 
-  if (props.json.err) return <Error {...props} pageTitle={t("page-title")} />;
+  if (props.json.err) return <Error {...props} pageTitle={t('page-title')} />
 
-  const url = `${process.env.NEXT_PUBLIC_WEBSOCK_URL}/${props.aps}/${props.side}/overview`;
-
-  const [overview, setOverview] = React.useState(props.data);
+  const [overview, setOverview] = React.useState(props.json)
+  const url = `${process.env.NEXT_PUBLIC_WEBSOCK_URL}/${props.aps}/overview`
   const { data, loading } = useData(url, {
     initialData: overview,
-    page: "overview",
-  });
-  React.useEffect(() => setOverview(data), [data]);
+    page: 'overview'
+  })
+  React.useEffect(() => setOverview(data), [data])
 
   // Dialog
-  const [id, setId] = React.useState(0);
-  const [open, setOpen] = React.useState(false);
+  const [id, setId] = React.useState(0)
+  const [open, setOpen] = React.useState(false)
 
-  const handleOperationId = (id) => {
-    setId(id);
-    setOpen(true);
-  };
+  const handleOperationId = id => {
+    setId(id)
+    setOpen(true)
+  }
 
   const handleCancel = () => {
-    setOpen(false);
-  };
+    setOpen(false)
+  }
 
-  const handleConfirm = async (card) => {
-    setOpen(false);
-    const url = `${process.env.NEXT_PUBLIC_BACKEND_URL}/${props.aps}/${props.side}/system/operation`;
+  const handleConfirm = async card => {
+    setOpen(false)
+    const url = `${process.env.NEXT_PUBLIC_BACKEND_URL}/${props.aps}/system/operation`
     const json = await fetch(url, {
-      method: "POST",
+      method: 'POST',
       headers: {
-        Authorization: "Bearer " + props.token,
-        "Content-Type": "application/json",
+        Authorization: 'Bearer ' + props.token,
+        'Content-Type': 'application/json'
       },
-      body: JSON.stringify({ card, id }),
-    });
-    setOpenMessage(true);
-    setResponse(json);
-  };
+      body: JSON.stringify({ card, id })
+    })
+    setOpenMessage(true)
+    setResponse(json)
+  }
 
   const handleDelete = async ({ card, index }) => {
-    const url = `${process.env.NEXT_PUBLIC_BACKEND_URL}/${props.aps}/${props.side}/system/queue/delete`;
+    const url = `${process.env.NEXT_PUBLIC_BACKEND_URL}/${props.aps}/system/queue/delete`
     const json = await fetch(url, {
-      method: "POST",
+      method: 'POST',
       headers: {
-        Authorization: "Bearer " + props.token,
-        "Content-Type": "application/json",
+        Authorization: 'Bearer ' + props.token,
+        'Content-Type': 'application/json'
       },
-      body: JSON.stringify({ card, index }),
-    });
-    setOpenMessage(true);
-    setResponse(json);
-  };
+      body: JSON.stringify({ card, index })
+    })
+    setOpenMessage(true)
+    setResponse(json)
+  }
+
+  // Message
+  const [openMessage, setOpenMessage] = React.useState(false)
+  const [response, setResponse] = React.useState(null)
+  const handleCloseMessage = (event, reason) => {
+    if (reason === 'clickaway') {
+      return
+    }
+    setOpenMessage(false)
+  }
 
   return (
-    <>
+    <Layout {...props} pageTitle={t('page-title')}>
       <Grid container spacing={1}>
         <Grid item xs={12} sm={6} md={8} xl={9}>
           <Grid
@@ -134,11 +144,15 @@ export default function Overview(props) {
         open={open}
         onCancel={handleCancel}
         onConfirm={handleConfirm}
-        // value={operation}
         id={id}
         minCard={1}
         maxCard={overview.definitions.cards}
       />
-    </>
-  );
+      <Message
+        open={openMessage}
+        response={response}
+        handleClose={handleCloseMessage}
+      />
+    </Layout>
+  )
 }
