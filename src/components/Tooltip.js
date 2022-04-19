9b9041a97234967d852908f924411b629acc03fc
Fabio Riva <f.riva@sotefin.ch>
2022-04-19 11:36:54 +0200

    Tooltip component style

--- src/components/Tooltip.js
+++ src/components/Tooltip.js
@@ -1,34 +1,16 @@
 import { styled } from '@mui/material/styles'
-// import { withStyles } from '@mui/styles'
 import Tooltip, { tooltipClasses } from '@mui/material/Tooltip'
 
-// const HtmlTooltip = withStyles({
-//   tooltip: {
-//     backgroundColor: 'rgb(88,88,88)',
-//     color: '#fff',
-//     maxWidth: 240,
-//     fontSize: 16
-//   }
-// })(Tooltip)
-
-// const HtmlTooltip = styled(Tooltip)({
-//   // tooltip: {
-//   backgroundColor: 'rgb(88,88,88)',
-//   color: '#fff',
-//   maxWidth: 240,
-//   fontSize: 16
-//   // }
-// })
-
-const HtmlTooltip = styled(({ className, ...props }) => (
-  <Tooltip {...props} classes={{ popper: className }} />
+const CustomTooltip = styled(({ className, ...props }) => (
+  <Tooltip {...props} arrow classes={{ popper: className }} />
 ))(({ theme }) => ({
+  [`& .${tooltipClasses.arrow}`]: {
+    color: theme.palette.common.black,
+  },
   [`& .${tooltipClasses.tooltip}`]: {
-    backgroundColor: theme.palette.common.white,
-    color: 'rgba(0, 0, 0, 0.87)',
-    boxShadow: theme.shadows[1],
+    backgroundColor: theme.palette.common.black,
     fontSize: 14
-  }
-}))
+  },
+}));
 
-export default HtmlTooltip
+export default CustomTooltip
