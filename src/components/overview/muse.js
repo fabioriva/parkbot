177cfea2645bc9919e31d3655b4ad6a98cbab9ba
Fabio Riva <f.riva@sotefin.ch>
2022-06-27 15:55:45 +0200

    aps muse: differentiate exit/rollback rights (for valet user)

--- src/components/overview/muse.js
+++ src/components/overview/muse.js
@@ -1,118 +1,118 @@
-import React from 'react'
-import useTranslation from 'next-translate/useTranslation'
-import Grid from '@mui/material/Grid'
-import Error from 'src/components/Error'
-import Layout from 'src/components/Layout'
-import Message from 'src/components/Message'
-import Device from 'src/components/overview/Device'
-import OperationDialog from 'src/components/overview/OperationDialog'
-import RollbackDialog from 'src/components/ConfirmDialog'
-import Queue from 'src/components/overview/Queue'
-import fetch from 'src/lib/fetch'
-import { useData } from 'src/lib/useWebSocket'
-import { ACTIONS, isAllowed } from '/src/constants/auth'
+import React from "react";
+import useTranslation from "next-translate/useTranslation";
+import Grid from "@mui/material/Grid";
+import Error from "src/components/Error";
+import Layout from "src/components/Layout";
+import Message from "src/components/Message";
+import Device from "src/components/overview/Device";
+import OperationDialog from "src/components/overview/OperationDialog";
+import RollbackDialog from "src/components/ConfirmDialog";
+import Queue from "src/components/overview/Queue";
+import fetch from "src/lib/fetch";
+import { useData } from "src/lib/useWebSocket";
+import { EXIT, ROLLBACK, isAllowed } from "/src/constants/auth";
 
-export default function Overview (props) {
-  const { t } = useTranslation('overview')
+export default function Overview(props) {
+  const { t } = useTranslation("overview");
 
-  if (props.json.err) return <Error {...props} pageTitle={t('page-title')} />
+  if (props.json.err) return <Error {...props} pageTitle={t("page-title")} />;
 
-  const [overview, setOverview] = React.useState(props.json)
-  const url = `${process.env.NEXT_PUBLIC_WEBSOCK_URL}/${props.aps}/overview`
+  const [overview, setOverview] = React.useState(props.json);
+  const url = `${process.env.NEXT_PUBLIC_WEBSOCK_URL}/${props.aps}/overview`;
   const { data, loading } = useData(url, {
     initialData: overview,
-    page: 'overview'
-  })
-  React.useEffect(() => setOverview(data), [data])
+    page: "overview",
+  });
+  React.useEffect(() => setOverview(data), [data]);
 
   // Dialog
-  const [id, setId] = React.useState(0)
-  const [open, setOpen] = React.useState(false)
+  const [id, setId] = React.useState(0);
+  const [open, setOpen] = React.useState(false);
 
-  const handleOperationId = id => {
-    setId(id)
-    setOpen(true)
-  }
+  const handleOperationId = (id) => {
+    setId(id);
+    setOpen(true);
+  };
 
   const handleCancel = () => {
-    setOpen(false)
-  }
+    setOpen(false);
+  };
 
-  const handleConfirm = async card => {
-    setOpen(false)
-    const url = `${process.env.NEXT_PUBLIC_BACKEND_URL}/${props.aps}/system/operation`
+  const handleConfirm = async (card) => {
+    setOpen(false);
+    const url = `${process.env.NEXT_PUBLIC_BACKEND_URL}/${props.aps}/system/operation`;
     const json = await fetch(url, {
-      method: 'POST',
+      method: "POST",
       headers: {
-        Authorization: 'Bearer ' + props.token,
-        'Content-Type': 'application/json'
+        Authorization: "Bearer " + props.token,
+        "Content-Type": "application/json",
       },
-      body: JSON.stringify({ card, id })
-    })
-    setOpenMessage(true)
-    setResponse(json)
-  }
+      body: JSON.stringify({ card, id }),
+    });
+    setOpenMessage(true);
+    setResponse(json);
+  };
 
   const handleDelete = async ({ card, index }) => {
-    const url = `${process.env.NEXT_PUBLIC_BACKEND_URL}/${props.aps}/system/queue/delete`
+    const url = `${process.env.NEXT_PUBLIC_BACKEND_URL}/${props.aps}/system/queue/delete`;
     const json = await fetch(url, {
-      method: 'POST',
+      method: "POST",
       headers: {
-        Authorization: 'Bearer ' + props.token,
-        'Content-Type': 'application/json'
+        Authorization: "Bearer " + props.token,
+        "Content-Type": "application/json",
       },
-      body: JSON.stringify({ card, index })
-    })
-    setOpenMessage(true)
-    setResponse(json)
-  }
+      body: JSON.stringify({ card, index }),
+    });
+    setOpenMessage(true);
+    setResponse(json);
+  };
 
   // Rollback
-  const [confirmOpen, setConfirmOpen] = React.useState(false)
-  const [confirmValue, setConfirmValue] = React.useState({})
+  const [confirmOpen, setConfirmOpen] = React.useState(false);
+  const [confirmValue, setConfirmValue] = React.useState({});
 
-  const handleActionConfirm = async conn => {
-    setConfirmOpen(true)
-    setConfirmValue(conn)
-  }
+  const handleActionConfirm = async (conn) => {
+    setConfirmOpen(true);
+    setConfirmValue(conn);
+  };
 
-  const handleAction = async conn => {
-    console.log('handleAction', conn)
+  const handleAction = async (conn) => {
+    console.log("handleAction", conn);
     // const url = `${process.env.NEXT_PUBLIC_BACKEND_URL}/${props.aps}/readArea`
-    const url = `${process.env.NEXT_PUBLIC_BACKEND_URL}/${props.aps}/writeArea`
-    const FALSE = Buffer.alloc(1, 0, 'hex')
-    const TRUE = Buffer.alloc(1, 1, 'hex')
-    const buffer = Buffer.alloc(1, TRUE, 'hex').toJSON()
+    const url = `${process.env.NEXT_PUBLIC_BACKEND_URL}/${props.aps}/writeArea`;
+    const FALSE = Buffer.alloc(1, 0, "hex");
+    const TRUE = Buffer.alloc(1, 1, "hex");
+    const buffer = Buffer.alloc(1, TRUE, "hex").toJSON();
     // const buffer = Buffer.allocUnsafe(2)
     // buffer.writeUInt16BE(123, 0)
     // buffer.toJSON()
-    console.log(buffer)
+    // console.log(buffer);
     const json = await fetch(url, {
-      method: 'POST',
+      method: "POST",
       headers: {
         // Authorization: 'Bearer ' + props.token,
-        'Content-Type': 'application/json'
+        "Content-Type": "application/json",
       },
-      body: JSON.stringify({ conn, buffer })
-    })
-    console.log(json)
-    setConfirmOpen(false)
-    setOpenMessage(true)
-    setResponse(json)
-  }
+      body: JSON.stringify({ conn, buffer }),
+    });
+    console.log(json);
+    setConfirmOpen(false);
+    setOpenMessage(true);
+    setResponse(json);
+  };
 
   // Message
-  const [openMessage, setOpenMessage] = React.useState(false)
-  const [response, setResponse] = React.useState(null)
+  const [openMessage, setOpenMessage] = React.useState(false);
+  const [response, setResponse] = React.useState(null);
   const handleCloseMessage = (event, reason) => {
-    if (reason === 'clickaway') {
-      return
+    if (reason === "clickaway") {
+      return;
     }
-    setOpenMessage(false)
-  }
+    setOpenMessage(false);
+  };
 
   return (
-    <Layout {...props} pageTitle={t('page-title')}>
+    <Layout {...props} pageTitle={t("page-title")}>
       <Grid container spacing={2}>
         <Grid item xs={12} sm={6} md={8}>
           <Grid container spacing={2}>
@@ -120,7 +120,7 @@ export default function Overview (props) {
               <Device
                 item={overview.devices[0]}
                 aps={props.aps}
-                auth={isAllowed(props.user, [ACTIONS])}
+                auth={isAllowed(props.user, [ROLLBACK])}
                 user={props.user}
                 loading={loading}
               />
@@ -129,7 +129,7 @@ export default function Overview (props) {
               <Device
                 item={overview.devices[1]}
                 aps={props.aps}
-                auth={isAllowed(props.user, [ACTIONS])}
+                auth={isAllowed(props.user, [ROLLBACK])}
                 user={props.user}
                 loading={loading}
               />
@@ -138,7 +138,7 @@ export default function Overview (props) {
               <Device
                 item={overview.devices[2]}
                 aps={props.aps}
-                auth={isAllowed(props.user, [ACTIONS])}
+                auth={isAllowed(props.user, [ROLLBACK])}
                 action={handleActionConfirm}
                 user={props.user}
                 loading={loading}
@@ -148,7 +148,7 @@ export default function Overview (props) {
               <Device
                 item={overview.devices[3]}
                 aps={props.aps}
-                auth={isAllowed(props.user, [ACTIONS])}
+                auth={isAllowed(props.user, [ROLLBACK])}
                 action={handleActionConfirm}
                 user={props.user}
                 loading={loading}
@@ -158,7 +158,7 @@ export default function Overview (props) {
         </Grid>
         <Grid item xs={12} sm={6} md={4}>
           <Queue
-            auth={isAllowed(props.user, [ACTIONS])}
+            auth={isAllowed(props.user, [EXIT])}
             data={overview.exitQueue}
             onDelete={handleDelete}
             onExit={() => handleOperationId(0)}
@@ -175,8 +175,8 @@ export default function Overview (props) {
         maxCard={overview.definitions.cards}
       />
       <RollbackDialog
-        dialogContent={t('action-rollback-confirm')}
-        dialogTitle={t('action-rollback')}
+        dialogContent={t("action-rollback-confirm")}
+        dialogTitle={t("action-rollback")}
         open={confirmOpen}
         value={confirmValue}
         onCancel={() => setConfirmOpen(false)}
@@ -188,5 +188,5 @@ export default function Overview (props) {
         handleClose={handleCloseMessage}
       />
     </Layout>
-  )
+  );
 }
--- src/constants/auth.js
+++ src/constants/auth.js
@@ -5,30 +5,33 @@
 // export const USER = 3
 
 // roles (page access)
-export const OVERVIEW = 'overview'
-export const MAP = 'map'
-export const RACKS = 'racks'
-export const CARDS = 'cards'
-export const HISTORY = 'history'
-export const STATISTICS = 'statistics'
-export const ALARMS = 'alarms'
-export const NOTIFICATIONS = 'notifications'
-export const DASHBOARD = 'dashboard'
-export const DEVICE = 'device'
-export const DSS = 'dss'
+export const OVERVIEW = "overview";
+export const MAP = "map";
+export const RACKS = "racks";
+export const CARDS = "cards";
+export const HISTORY = "history";
+export const STATISTICS = "statistics";
+export const ALARMS = "alarms";
+export const NOTIFICATIONS = "notifications";
+export const DASHBOARD = "dashboard";
+export const DEVICE = "device";
+export const DSS = "dss";
 
 // rights (actions)
-export const ACTIONS = 'actions'
-export const DIAGNOSTIC = 'diagnostic'
-export const EDIT_CARD = 'edit-card'
-export const EDIT_STALL = 'edit-stall'
+export const ACTIONS = "actions"; // deprecated
+export const DIAGNOSTIC = "diagnostic";
+export const EDIT_CARD = "edit-card";
+export const EDIT_STALL = "edit-stall";
+export const ENTRY = "entry";
+export const EXIT = "exit";
+export const ROLLBACK = "rollback";
 
 export const hasRole = (user, roles) =>
-  roles.some(role =>
+  roles.some((role) =>
     user.roles !== undefined ? user.roles.includes(role) : false
-  )
+  );
 
 export const isAllowed = (user, rights) =>
-  rights.some(right =>
+  rights.some((right) =>
     user.rights !== undefined ? user.rights.includes(right) : false
-  )
+  );
